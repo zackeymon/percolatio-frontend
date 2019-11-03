ebc08324cefb4cd5aa60a0d28608322f3528bfdc
monty <amonteolivam@gmail.com>
2019-11-03 19:01:45 +0000

    extend grant list ui

---
+++ src/components/GrantList/GrantList.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ListPagination from 'components/ListPagination';
+import GrantPreview from './GrantPreview';
+
+const GrantList = (props) => {
+  if (!props.grants) {
+    return (
+      <div className="article-preview">Loading...</div>
+    );
+  }
+
+  if (props.grants.length === 0) {
+    return (
+      <div className="article-preview">
+        There are no grants in Percolatio... yet.
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {
+        props.grants.map((grant) => (
+          <GrantPreview grant={grant} key={grant.slug} />
+        ))
+      }
+
+      <ListPagination
+        pager={props.pager}
+        grantsCount={props.grantsCount}
+        currentPage={props.currentPage}
+      />
+    </div>
+  );
+};
+
+export default GrantList;
--- src/components/GrantList/GrantPreview.jsx
+++ src/components/GrantList/GrantPreview.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button, Card } from 'antd';
+import {
+  Button, Card, Avatar, Row, Col,
+} from 'antd';
 import 'antd/dist/antd.css';
 
 const mapDispatchToProps = () => ({
@@ -17,9 +19,56 @@ const GrantPreview = (props) => {
 
   return (
 
-    <Card title={title} extra={<Button type="primary">Apply</Button>} style={{ marginTop: '50px' }}>
-      <p>{`slug: ${slug}`}</p>
-      <p>{`author: ${foundation.name}`}</p>
+    <Card
+      title={title}
+      extra={(
+        <div>
+          <Button href="/apply" type="primary">Apply</Button>
+          {' '}
+          <Button href={`/grants/${slug}`}>See More</Button>
+        </div>
+)}
+      style={{ marginTop: '50px' }}
+    >
+      <div>
+        <Row type="flex" justify="space-around" align="middle">
+          <Col span={5}>
+            {<Avatar
+              shape="circle"
+              size={80}
+              icon="user"
+            />}
+          </Col>
+          <Col span={13}>
+            <ul>
+              <li>
+              Foundation:
+                {' '}
+                {`${foundation.name}`}
+              </li>
+              <li>
+              Description:
+                {''}
+                {`${grant.description}`}
+              </li>
+            </ul>
+
+          </Col>
+          <Col span={6}>
+            <Row>
+              Deadline: 17-02-2020
+            </Row>
+
+            <Row>
+              Amount:
+              {' '}
+              {`${grant.amount}`}
+            </Row>
+
+          </Col>
+        </Row>
+      </div>
+
 
     </Card>
   );
