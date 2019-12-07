29f1b72bd0365888a4845df944de1ee9161e0e80
zackeymon <zixux00@gmail.com>
2019-12-07 11:30:36 +0000

    Foundation Form

--- src/reducers/foundation.js
+++ src/reducers/foundation.js
@@ -1,6 +1,8 @@
 import {
   FOUNDATION_PAGE_UNLOADED,
   FOUNDATION_PAGE_LOADED,
+  NEW_FOUNDATION,
+  ASYNC_START,
 } from 'constants/actionTypes';
 
 export default (state = {}, action) => {
@@ -12,6 +14,17 @@ export default (state = {}, action) => {
       };
     case FOUNDATION_PAGE_UNLOADED:
       return {};
+    case ASYNC_START:
+      if (action.subtype === NEW_FOUNDATION) {
+        return { ...state, isSubmittingForm: true };
+      }
+      return state;
+    case NEW_FOUNDATION:
+      return {
+        ...state,
+        isSubmittingForm: false,
+        errors: action.error ? action.payload.errors : null,
+      };
     default:
       return state;
   }
