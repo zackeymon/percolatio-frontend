9d6ecc1d481b2b64c928c1873b36ec355fc943b9
Zack <zixux00@gmail.com>
2019-12-08 09:15:18 +0000

    Bugfix/dashboard (#13)
    * Now it's not longer that hacky. Implemented happy path only
    
    * Surface grant at Overview dashboard

--- src/components/Dashboard/FoundationTab.jsx
+++ src/components/Dashboard/FoundationTab.jsx
@@ -1,42 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import agent from 'agent';
-import { DASHBOARD_FOUNDATIONS_LOADED } from 'constants/actionTypes';
 
-const mapStateToProps = (state, ownProps) => {
-  const { grantsForFoundation } = state.dashboard;
-  return {
-    grants: grantsForFoundation && ownProps.foundation.name in grantsForFoundation
-      ? grantsForFoundation[ownProps.foundation.name]
-      : null,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => ({
-  onLoad: (payload) => dispatch({
-    type: DASHBOARD_FOUNDATIONS_LOADED, payload,
-  }),
-});
-
-class FoundationTab extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props.onLoad(agent.Grants.byFoundation(this.props.foundation.name));
-  }
-
-  render() {
-    const { foundation, grants } = this.props;
-    return (
-      <>
-        <h1>{foundation.name}</h1>
-        <p>{foundation.description}</p>
-        {grants && grants.map((grant) => (
+const FoundationTab = (props) => {
+  const { foundation, grants } = props;
+  return (
+    <>
+      <h1>{foundation.name}</h1>
+      <p>{foundation.description}</p>
+      {grants.length > 0
+        ? grants.map((grant) => (
           <p key={grant.slug}>
             {grant.slug}
           </p>
-        ))}
-      </>
-    );
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(FoundationTab);
+        ))
+        : <p>This foundation has no grant yet</p>}
+    </>
+  );
+};
+
+export default FoundationTab;
